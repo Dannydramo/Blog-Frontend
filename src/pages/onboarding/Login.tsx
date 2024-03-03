5efdc27dfed2cae467b86ac15aac6ed1be314c68
Dannydramo <alumandudaniel217@gmail.com>
2024-03-03 10:53:23 -0800

    made changes

--- src/pages/onboarding/Login.tsx
+++ src/pages/onboarding/Login.tsx
@@ -63,29 +63,30 @@ const Login = () => {
             `${import.meta.env.VITE_APP_API_URL}/oauth/google`,
             "_blank"
         );
+        console.log(googleAuthWindow);
 
-        // Listen for messages from the opened window
-        window.addEventListener("message", (event) => {
-            // Check if the message is from the opened window
-            if (event.source === googleAuthWindow) {
-                // Assuming the message data includes status, token, and redirectUrl
-                const { status, token, redirectUrl } = event.data;
-                if (status === "success") {
-                    // Handle successful authentication
-                    console.log("Received token:", token);
-                    // Optionally, store the token in localStorage or a cookie
-                    // Redirect to the specified URL
-                    if (redirectUrl) {
-                        window.location.href = redirectUrl;
-                    }
-                } else {
-                    // Handle unsuccessful authentication
-                    console.error("Authentication failed");
-                }
-                // Close the authentication window after receiving the message
-                googleAuthWindow?.close();
-            }
-        });
+        // // Listen for messages from the opened window
+        // window.addEventListener("message", (event) => {
+        //     // Check if the message is from the opened window
+        //     if (event.source === googleAuthWindow) {
+        //         // Assuming the message data includes status, token, and redirectUrl
+        //         const { status, token, redirectUrl } = event.data;
+        //         if (status === "success") {
+        //             // Handle successful authentication
+        //             console.log("Received token:", token);
+        //             // Optionally, store the token in localStorage or a cookie
+        //             // Redirect to the specified URL
+        //             if (redirectUrl) {
+        //                 window.location.href = redirectUrl;
+        //             }
+        //         } else {
+        //             // Handle unsuccessful authentication
+        //             console.error("Authentication failed");
+        //         }
+        //         // Close the authentication window after receiving the message
+        //         googleAuthWindow?.close();
+        //     }
+        // });
     };
 
     return (
